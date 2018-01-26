02ea5526b1d0191831fc766d577f26584a9cc22e
3dhardgamer <artur.turbas@gmail.com>
2018-01-26 20:23:08 -0200

    Will only create the database

--- db_management.py
+++ db_management.py
@@ -1,7 +1,24 @@
-import pandas as pd
-import sqlalchemy
-import pytz
+from sqlalchemy import *
 
 
-def upload_to_db(symbol1, symbol2):
+def create_db(symbol1, symbol2):
+    '''
+    create_db():
+    creates the database for the given pair (symbol1/symbol2)
+    '''
+
     db_uri = 'sqlite:///{}_{}.sqlite'.format(symbol1, symbol2)
+    engine = create_engine(db_uri, echo = False)
+
+    metadata = MetaData(engine)
+
+    db_session = Table(db_uri, metadata,
+                Column('date', DateTime, primary_key = True),
+                Column('open', Float),
+                Column('high', Float),
+                Column('low', Float),
+                Column('close', Float),
+                Column('volume', Float),)
+
+    db_session.create()
+
