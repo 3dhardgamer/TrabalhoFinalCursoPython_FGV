bb084f1f0ce8691087d5e15d3ea8676fa113af20
3dhardgamer <artur.turbas@gmail.com>
2018-01-27 07:49:07 -0200

    Script that verify if db exists, and then populates the db.

--- create_db.py
+++ create_db.py
@@ -1,24 +1,3 @@
-from sqlalchemy import *
+from capturer import Capturer
 
-
-def create_db(symbol1, symbol2):
-    '''
-    create_db():
-    creates the database for the given pair (symbol1/symbol2)
-    '''
-
-    db_uri = 'sqlite:///{}_{}.sqlite'.format(symbol1, symbol2)
-    engine = create_engine(db_uri, echo = False)
-
-    metadata = MetaData(engine)
-
-    db_session = Table(db_uri, metadata,
-                Column('date', DateTime, primary_key = True),
-                Column('open', Float),
-                Column('high', Float),
-                Column('low', Float),
-                Column('close', Float),
-                Column('volume', Float),)
-
-    db_session.create()
 
